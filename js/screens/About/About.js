810beb09e40c267a1cdb1d337af8482363d40c53
Pamela Chong <pjchong91@gmail.com>
2018-08-30 13:32:27 -0700

    Readded Views over scrollviews

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -7,34 +7,35 @@ import PropTypes from "prop-types";
 
 const About = conducts => {
   return (
-    // <View style={styles.container}>
-    <ScrollView style={styles.container}>
-      <Image
-        source={require("./../../assets/r10_logo.png")}
-        style={styles.logo}
-      />
-      <View style={styles.divider} />
-      <Text style={[styles.bodyText, Styles.font]}>
-        R10 is a conference that focuses on just about any topic related to dev.
-      </Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Image
+          source={require("./../../assets/r10_logo.png")}
+          style={styles.logo}
+        />
+        <View style={styles.divider} />
+        <Text style={[styles.bodyText, Styles.font]}>
+          R10 is a conference that focuses on just about any topic related to
+          dev.
+        </Text>
 
-      <Text style={[styles.headerText, Styles.font]}>Date & Venue </Text>
+        <Text style={[styles.headerText, Styles.font]}>Date & Venue </Text>
 
-      <Text style={[styles.bodyText, Styles.font]}>
-        The R10 conference will take place on September 18, 2018 in Vancouver,
-        BC.
-      </Text>
+        <Text style={[styles.bodyText, Styles.font]}>
+          The R10 conference will take place on September 18, 2018 in Vancouver,
+          BC.
+        </Text>
 
-      <Text style={[styles.headerText, Styles.font]}>Code of Conduct</Text>
+        <Text style={[styles.headerText, Styles.font]}>Code of Conduct</Text>
 
-      {conducts.conducts.map(conduct => (
-        <ConductItem conduct={conduct} key={conduct.id} />
-      ))}
+        {conducts.conducts.map(conduct => (
+          <ConductItem conduct={conduct} key={conduct.id} />
+        ))}
 
-      <View style={styles.divider} />
-      <Text style={[styles.bodyText, Styles.font]}>© RED Academy 2018</Text>
-    </ScrollView>
-    // </View>
+        <View style={styles.divider} />
+        <Text style={[styles.bodyText, Styles.font]}>© RED Academy 2018</Text>
+      </ScrollView>
+    </View>
   );
 };
 
